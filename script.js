375a24df8c7d2d9a82d9b4eb3888ef08c30a1299
Gursharan Ahir <ahir.sharan@gmail.com>
2021-07-05 01:02:37 +0530

    Added 24 hrs deletion

---
+++ script.js
@@ -0,0 +1,25 @@
+const File = require('./models/file')
+const fs = require('fs')
+const connectDB = require('./config/db')
+
+connectDB();
+
+async function fetchData() {
+    const pastDate = new Date(Date.now() - (24 * 60 * 60 * 1000));
+    const files = await File.find({ createdAt: { $lt: pastDate } });
+
+    if (files.length) {
+        for (const file of files) {
+            try {
+                fs.unlinkSync(file.path);
+                await file.remove();
+                console.log(`Succesfully deleted ${file.filename}`);
+            } catch (err) {
+                console.log(`Error while deleting file ${err}`);
+            }
+        }
+        console.log('Job Done');
+    }
+}
+
+fetchData().then(process.exit());
