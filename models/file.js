0c45a005b54ec50c3a6f6026035a3f05a99a9594
Gursharan Ahir <ahir.sharan@gmail.com>
2021-07-13 16:07:27 +0530

    removed extra schema fields

--- models/file.js
+++ models/file.js
@@ -6,8 +6,6 @@ const fileSchema = new Schema({
     path: { type: String, required: true },
     size: { type: Number, required: true },
     uuid: { type: String, required: true },
-    sender: { type: String, required: false },
-    receiver: { type: String, required: false },
 }, { timestamps: true });
 
 module.exports = mongoose.model('File', fileSchema);
