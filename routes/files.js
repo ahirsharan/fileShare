661dd3e778155d25daf35e698884d0c138427600
Gursharan Ahir <ahir.sharan@gmail.com>
2021-07-13 15:39:06 +0530

    removed unecessary stuff

--- routes/files.js
+++ routes/files.js
@@ -12,7 +12,7 @@ let storage = multer.diskStorage({
     },
 });
 
-let upload = multer({ storage, limits: { fileSize: 1000000 * 100 }, }).single('myfile'); //100mb
+let upload = multer({ storage, limits: { fileSize: 1024 * 1024 * 10 }, }).single('myfile'); //10MB
 
 router.post('/', (req, res) => {
     upload(req, res, async(err) => {
