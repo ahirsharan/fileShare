bcf3f6aa4dcb6ee1c8f6df92c1ee5f8addd80c4c
Gursharan Ahir <ahir.sharan@gmail.com>
2022-02-24 22:08:31 -0800

    new n

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ connectDB();
 //Cors
 const corsOptions = {
     //origin: String(`"${process.env.ALLOWED_CLIENT}"`)
-    origin: "https://ahirsharan.github.io"
+    origin: ["https://ahirsharan.github.io", "https://nahir29.github.io"],
 }
 
 // Allowing backend connection with only our fronted url (can be a localhost)
